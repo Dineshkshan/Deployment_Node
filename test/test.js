3a28314486983f178328dd36931c3f12596f9285
Dineshkshan <dineshk.shan@gmail.com>
2020-06-10 16:51:17 +0530

    console in test is removed

--- test/test.js
+++ test/test.js
@@ -3,7 +3,7 @@ var request = require('request');
 
 it('Main page content', function(done) {
     request('http://localhost:5000' , function(error, response, body) {
-        console.log(response.body);
+        //console.log(response.body);
         expect(body).to.equal("Hello world!!!!");
         done();
     });
